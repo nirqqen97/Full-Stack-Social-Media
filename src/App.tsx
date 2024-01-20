45fca9e575f8e6b7abdefdc379863101275dd2d3
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2024-01-20 20:49:40 +0200

    setup + create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 
+import { Home } from "./_root/pages/Home";
+import { AuthLayout } from "./_auth/forms/AuthLayout";
+import { SignInForm } from "./_auth/forms/SignInForm";
+import { SignUpForm } from "./_auth/forms/SignUpForm";
+import { RootLayout } from "./_root/pages/RootLayout";
+
 export const App = () => {
-  return <main className=""></main>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        //* Public Route
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        //* Private Route
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
